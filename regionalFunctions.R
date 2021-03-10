eebba0ce26970ad24fa49535f591ca7ad0b72a04
lumaya630 <lumaya630@gmail.com>
2021-03-10 07:13:08 -0800

    regional analysis

---
+++ regionalFunctions.R
@@ -0,0 +1,45 @@
+# =====================
+# CALC NDOCS PER REGION
+# =====================
+# function that gets number of documents containing asia
+# @param: dat - dataframe to be analyzed
+# @param: region - vector of keywords for each region
+
+nDocs <- function(dat, region) {
+  idx <- which(colnames(dat) %in% region)
+  df <- dat[ ,idx]
+  df$total <- rowSums(df)
+  
+  # num documents containing regional keywords
+  df$has_region <- df$total > 0
+  ndocs <- sum(df$has_region)
+  
+  return(ndocs)
+}
+
+# =========================
+# CALC SENTIMENT FOR REGION
+# =========================
+# function that calculates sentiment of a region
+# @param: dataframe
+# @param: region - vector of keywords for region
+# @returns: ratio of positive to positive & negative words for dataframe
+
+regionSentiment <- function(dat, region) {
+  # create regional df
+  idx <- which(colnames(dat) %in% region)
+  df <- dat[ ,idx]
+  df$total <- rowSums(df)
+  df$has_region <- df$total > 0
+  
+  # get index of observations w/ dataframe
+  idx <- which(df$has_region == 1)
+  df <- dat[idx,]
+  sent <- calcSentiment(df) #helper function to calculate sentiment of df
+  
+  return(sent)
+}
+
+
+
+
